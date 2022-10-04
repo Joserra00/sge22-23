e24f9bdf074042ab3749b6a6d60199e05af06bb8
Joserra00 <joseramongarrido175@gmail.com>
2022-10-04 10:41:14 +0000

    nuevo push

--- runescape/models/models.py
+++ runescape/models/models.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class runescape(models.Model):
-#     _name = 'runescape.runescape'
-#     _description = 'runescape.runescape'
+class runescape(models.Model):
+    _name = 'runescape.player'
+    _description = 'Player of the game'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(required=True)
+    password = fields.Char()
+    avatar = fields.Image(max_width = 200, max_height=200)
+
+
