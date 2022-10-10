aa1461c454998a24f47843210e801ed3744a67a1
Joserra00 <joseramongarrido175@gmail.com>
2022-10-10 08:31:29 +0000

    commit

--- runescape/models/models.py
+++ runescape/models/models.py
@@ -3,12 +3,27 @@
 from odoo import models, fields, api
 
 
-class runescape(models.Model):
+class player(models.Model):
     _name = 'runescape.player'
     _description = 'Player of the game'
 
     name = fields.Char(required=True)
     password = fields.Char()
     avatar = fields.Image(max_width = 200, max_height=200)
+    world = fields.Many2one('runescape.world')
+
+class world(models.Model):
+    _name = 'runescape.world'
+    _description = 'Runescape World'
+
+    name = fields.Char(required=True)
+    player = fields.One2many('runescape.player','world')
+
+class armor(models.Model):
+    _name = 'runescape.armor'
+    _description = 'Runescape armor'
 
 
+class material (models.Model):
+    _name = 'runescape.material'
+    _description = 'Runescape material'
