a7ab664a867e2a9464d550876c330361231ddade
Joserra00 <joseramongarrido175@gmail.com>
2022-10-11 10:41:46 +0000

    actu

--- runescape/models/models.py
+++ runescape/models/models.py
@@ -10,20 +10,30 @@ class player(models.Model):
     name = fields.Char(required=True)
     password = fields.Char()
     avatar = fields.Image(max_width = 200, max_height=200)
-    world = fields.Many2one('runescape.world')
+    armor = fields.Many2one('runescape.armor')
+    coins = fields.Integer(default=0)
+    sword = fields.Many2one('runescape.sword')
 
-class world(models.Model):
-    _name = 'runescape.world'
-    _description = 'Runescape World'
 
-    name = fields.Char(required=True)
-    player = fields.One2many('runescape.player','world')
 
 class armor(models.Model):
     _name = 'runescape.armor'
     _description = 'Runescape armor'
 
+    material=fields.Selection([('1','Iron'),('2','Mithril'),('3','Rune')])
+    defense=fields.Integer()
 
-class material (models.Model):
+class sword(models.Model):
+    _name = 'runescape.sword'
+    _description = 'Runescape sword'
+
+    material=fields.Selection([('1','Iron'),('2','Mithril'),('3','Rune')])
+    damage=fields.Integer()
+
+
+class material(models.Model):
     _name = 'runescape.material'
     _description = 'Runescape material'
+
+    type=fields.Selection([('1','Iron'),('2','Mithril'),('3','Rune')])
+
